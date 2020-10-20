6d8b9c6f7621a006cc22041eab1faf459713f0dd
HARSHIT YADAV <56081331+Harshit-1999@users.noreply.github.com>
2020-10-20 18:32:32 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Parallax, ParallaxLayer } from 'react-spring/addons'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 
 // Little helpers ...
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
